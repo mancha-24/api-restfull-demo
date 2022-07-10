c3d3347f33de347eed3d075259fd729d44068566
Manu <manuel.chavez@accenture.com>
2022-07-10 13:58:38 -0500

    Creating DTO

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Repositories;
@@ -27,19 +28,47 @@ namespace SocialMediaApi.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+
+            var postDto = posts.Select(x => new PostDto
+            {
+                PostId = x.PostId,
+                Date = x.Date,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+            });
+
+            return Ok(postDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post
+            {
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
+            
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
---
+++ SocialMedia.Core/DTOs/PostDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.DTOs
+{
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public string? Image { get; set; }
+    }
+}
